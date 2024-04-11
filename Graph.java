28cc369a5f208162ae25fc9ff7a9841f1e103b0d
CreativeSeba <112626875+CreativeSeba@users.noreply.github.com>
2024-04-11 14:16:24 +0200

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -20,10 +20,10 @@ public class Graph {
     }
 
     public Node add_node(String name) {
-            Node node = new Node(name);
-            nodes.add(node);
-            return node;
-        }
+        Node node = new Node(name);
+        nodes.add(node);
+        return node;
+    }
 
 
     public Node remove_node(int id) {
@@ -97,5 +97,44 @@ public class Graph {
             System.out.println("Edge not found");
         }
         return null;
+    }
+    public void findRoad(int id1, int id2){
+        ArrayList<Edge> firstEdges = new ArrayList<>();
+        ArrayList<Edge> nextEdges = new ArrayList<>();
+        ArrayList<Integer> roads = new ArrayList<>();
+        int roadLength=0;
+        if(id1==id2){
+            System.out.println("Nodes have to be different");
+        }
+        else{
+            for(Edge edge : edges){
+                if(edge.id1==id1 || edge.id2==id1){
+                    firstEdges.add(edge);
+                }
+            }
+            int i = 0;
+            while(true){
+                Edge currentEdge = firstEdges.get(i);
+                roadLength+=currentEdge.weight;
+                if(i==0){
+                    for(Edge edge : edges){
+                        if(edge.id1==currentEdge.id2||edge.id2==currentEdge.id2){
+                            nextEdges.add(edge);
+                        }
+                    }
+                    Edge smallestValue;
+                    for (int j = 0; j < nextEdges.size(); j++) {
+                        smallestValue.weight = nextEdges.get(j).weight;
+                        if (smallestValue.weight > nextEdges.get(j++).weight) {
+                            smallestValue.weight = nextEdges.get(j++).weight;
+                        }
+                    }
+//                    currentEdge = 
+                }
+            }
+        }
+        for(Edge edge : firstEdges) {
+            System.out.println(edge.id1 + " " + edge.id2);
+        }
     }
 }
