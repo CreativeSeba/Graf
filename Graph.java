4965353e590453ac25e3045dbcfe8829dab16eb6
CreativeSeba <112626875+CreativeSeba@users.noreply.github.com>
2024-04-18 14:15:28 +0200

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,8 +1,6 @@
 package pack;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Collections;
 public class Graph {
     ArrayList<Node> nodes = new ArrayList<>();
     ArrayList<Edge> edges = new ArrayList<>();
@@ -88,81 +86,29 @@ public class Graph {
         }
         return null;
     }
-
-    public void findRoad(int sourceId, int destinationId) {
-        HashMap<Integer, Integer> distance = new HashMap<>(); // Stores the shortest distance from the source to each node
-        HashMap<Integer, Edge> previousEdge = new HashMap<>(); // Stores the previous edge on the shortest path to each node
-        HashSet<Integer> visited = new HashSet<>(); // Stores the visited nodes
-
-        for (Node node : nodes) {
-            distance.put(node.id, Integer.MAX_VALUE);
-            previousEdge.put(node.id, null);
+    public void find_road(int id1, int id2){
+        if(edges.isEmpty()){
+            System.out.println("Zbiór wierzchołków jest pusty");
         }
-
-// Distance from source to itself is 0
-        distance.put(sourceId, 0); // Add this line
-
-        while (!visited.contains(destinationId)) {
-            int currentId = -1;
-            int minDistance = Integer.MAX_VALUE;
-            // Find the node with the shortest distance that has not been visited yet
-            for (Node node : nodes) {
-                if (!visited.contains(node.id) && distance.get(node.id) < minDistance) {
-                    minDistance = distance.get(node.id);
-                    currentId = node.id;
+        else{
+            ArrayList<Node> Q = nodes;
+            ArrayList<Edge> vEdges = new ArrayList<>();
+            HashMap<Node, Integer> vMap = new HashMap<>();
+            for(Node node : nodes){
+                if(node.id==id1){
+                    Q.remove(node);
+                    break;
                 }
             }
-
-            if (currentId == -1) {
-                // No reachable nodes left, exit the loop
-                break;
-            }
-
-            visited.add(currentId);
-
-            // Update distances to neighbors of the current node
-            for (Edge edge : edges) {
-                if (edge.id1 == currentId && !visited.contains(edge.id2)) {
-                    int newDistance = distance.get(currentId) + edge.weight;
-                    if (newDistance < distance.get(edge.id2)) {
-                        distance.put(edge.id2, newDistance);
-                        previousEdge.put(edge.id2, edge);
-                    }
-                } else if (edge.id2 == currentId && !visited.contains(edge.id1)) {
-                    int newDistance = distance.get(currentId) + edge.weight;
-                    if (newDistance < distance.get(edge.id1)) {
-                        distance.put(edge.id1, newDistance);
-                        previousEdge.put(edge.id1, edge);
-                    }
+            for(Edge edge : edges){
+                if(edge.id1 == id1 || edge.id2 == id2){
+                    vEdges.add(edge);
                 }
             }
+            
         }
-
-        // Reconstruct the shortest path from source to destination
-        ArrayList<Edge> shortestPath = new ArrayList<>();
-        Edge currentEdge = previousEdge.get(destinationId);
-        while (currentEdge != null) {
-            shortestPath.add(currentEdge);
-            if (currentEdge.id1 == destinationId) {
-                destinationId = currentEdge.id2;
-            } else {
-                destinationId = currentEdge.id1;
-            }
-            currentEdge = previousEdge.get(destinationId);
-        }
-        Collections.reverse(shortestPath);
-
-        // Print the shortest path and its length
-        if (!shortestPath.isEmpty()) {
-            System.out.println("\nShortest path:\n");
-            int road = 0;
-            for (Edge edge : shortestPath) {
-                road+=edge.weight;
-                System.out.println("Edge id1: " + edge.id1 + " | id2: " + edge.id2 + " | weight " + edge.weight);
-            }
-            System.out.println("\nShortest path length: " + road);
-        } else {
-            System.out.println("No path found between the nodes.");
-        }
+    }
+    private void shortestWay(){
+        
     }
 }
