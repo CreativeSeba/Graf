d8b54e41d9befd9fc6f10f19143b540287c71e5c
CreativeSeba <112626875+CreativeSeba@users.noreply.github.com>
2024-04-25 21:57:08 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,6 +10,8 @@ public class Main {
         Node node2 = graph.addNode("Node 2");
         Node node3 = graph.addNode("Node 3");
         Node node4 = graph.addNode("Node 4");
+        Node node5 = graph.addNode("Node 5");
+        Node node6 = graph.addNode("Node 6");
 
         graph.addEdge(1, node0.id, node1.id);
         graph.addEdge(5, node1.id, node2.id);
@@ -17,6 +19,8 @@ public class Main {
         graph.addEdge(4, node2.id, node3.id);
         graph.addEdge(3, node0.id, node1.id);
         graph.addEdge(5, node1.id, node4.id);
+        graph.addEdge(5, node5.id, node6.id);
+
 
         graph.printGraph();
         graph.findRoad(node3.id, node4.id);
